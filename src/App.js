e94a1d1d89db0223f031161ec003ca020d28bcec
patriot <patriotina@gmail.com>
2020-10-14 18:43:21 +0400

    #homework - part 1

--- src/App.js
+++ src/App.js
@@ -1,57 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
+
 import './style.css';
 
 function App() {
   return (
-    <div>
-          <header className="header">
-        <img
-            src={logo}
-            alt="Logo Space X"
-            className="logo"
-        />
-        <nav className="main-nav nav">
-          <ul className="list">
-            <li className="item">
-              <a href="#" className="item-link">Falcon 1</a>
-            </li>
-            <li className="item">
-              <a href="#" className="item-link">Falcon 9</a>
-            </li>
-            <li className="item">
-              <a href="#" className="item-link">Falcon Heavy</a>
-            </li>
-            <li className="item">
-              <a href="#" className="item-link">Updates</a>
-            </li>
-          </ul>
-        </nav>
-        <nav className="secondary-nav">
-          <ul className="list">
-            <li className="item">
-              <a href="#" className="item-link">Home</a>
-            </li>
-            <li className="item">
-              <a href="calendar.html" className="item-link">Calendar</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <section class="main">
-      <h1 class="title">
-        Falcon 1
-      </h1>
-
-      <div class="video-container">
-        <video class="video" autoPlay loop muted src="./video/moon.mp4"></video>
-
-      </div>
-      </section>
-
-  </div>
-
+    <React.Fragment>
+      <Header />  
+      <Main />  
+      <Footer />
+  </React.Fragment>
   );
 }
 
